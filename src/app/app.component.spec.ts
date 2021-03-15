3acfad144c1ce3d06cc20d3cb11b0d44a410ef36
Daniel Muñoz <danmunoz@sistecredito.com>
2021-03-15 10:12:20 -0500

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,31 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { AppComponent } from "./app.component";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it("Cuando se inicie la aplicación, debe crear el componente AppComponent", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pet-book'`, () => {
+  it(`Cuando inicie la aplicación debe tener de título 'pet-book'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pet-book');
+    expect(app.title).toEqual("pet-book");
   });
 
-  it('should render title', () => {
+  it("Cuando inicie la aplicación debe renderizar el compomente raíz", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('pet-book app is running!');
+    expect(compiled.querySelector("#root-component")).not.toBeNull();
   });
 });
--- src/app/image-gallery/image-gallery.component.spec.ts
+++ src/app/image-gallery/image-gallery.component.spec.ts
@@ -1,16 +1,32 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { GalleryComponent } from './image-gallery.component';
+import { GalleryComponent } from "./image-gallery.component";
+import { ImageService } from "../image.service";
+import { FilterimagesPipe } from '../filterimages.pipe';
 
-describe('ImageGalleryComponent', () => {
+describe("ImageGalleryComponent", () => {
   let component: GalleryComponent;
   let fixture: ComponentFixture<GalleryComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GalleryComponent ]
-    })
-    .compileComponents();
+      declarations: [GalleryComponent, FilterimagesPipe],
+      providers: [
+        {
+          provide: ImageService,
+          useValue: {
+            getImages: () => {
+              return [
+                {
+                  id: 1,
+                  brand: "perro",
+                  url: "assets/images/perro1.jpg",
+                },
+              ];
+            },
+          },
+        },
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +35,37 @@ describe('ImageGalleryComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("Cuando se inicie la aplicación, debe crear el componente", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("Cuando se cargue el componente el tamaño de la lista debe ser uno", () => {
+    expect(component.allImages.length).toEqual(1);
+  });
+
+  it("Cuando se cargue el componente la lista debe contener el perro con id 1", () => {
+    component.allImages.forEach((item) => {
+      expect(item.id).toEqual(1);
+      expect(item.brand).toEqual("perro");
+      expect(item.url).toEqual("assets/images/perro1.jpg");
+    });
+  });
+
+  it("Cuando se cargue el componente debe tener 3 botones con los valores All, Perro y Gato", () => {
+    const buttons = fixture.nativeElement.querySelectorAll(
+      "#filterButtons .btn"
+    );
+    expect(buttons).toBeDefined();
+    const innerTexts = ["All", "Perro", "Gato"];
+    [].forEach.call(innerTexts, (text) => {
+      let index = 0;
+      for (let i = 0; i < buttons.length; i++) {
+        if (buttons[i].innerText === text) {
+          index = i;
+          break;
+        }
+      }
+      expect(index).toBeGreaterThanOrEqual(0);
+    });
   });
 });
